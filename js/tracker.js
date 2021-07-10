88b16ce43cbb7128151fdaef908b2a49620bbe3c
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-07-10 16:44:35 +0200

    Declared the myLeads array and initialized the inputEl element

--- js/tracker.js
+++ js/tracker.js
@@ -1,9 +1,15 @@
+// To store the leads
+let myLeads = [];
+
+const inputEl = document.querySelector("#input-el");
+
 // For the SAVE INPUT button
 
 // Getting the SAVE INPUT button element
-let InputBtn = document.getElementById("input-btn");
+const InputBtn = document.getElementById("input-btn");
 
 // Adding an event listener to it
 InputBtn.addEventListener("click", function() {
     console.log("Input saved!");
 });
+
